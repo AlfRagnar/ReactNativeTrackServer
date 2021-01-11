087e080c3a005d5f649829973c8f8133bb7d98a2
Alf-Ragnar Wirkola Masternes <alf-ragnar.wirkola@edunord.se>
2021-01-11 20:55:49 +0100

    Chapter 14 upto episode 20

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const User = mongoose.model("User");
 
 const router = express.Router();
@@ -9,9 +10,33 @@ router.post("/signup", async (req, res) => {
     try {
         const user = new User({ email, password });
         await user.save();
-        res.send("You made a post request");
+        console.log(req.body); // check if a user has been created
+
+        const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+        res.send({ token });
     } catch (err) {
         return res.status(422).send(err.message);
+    }
+});
+
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res
+            .status(422)
+            .send({ error: "Must provide a valid email or password" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).send({ error: "Invalid password or email" });
+    }
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+        res.send({ token });
+    } catch (error) {
+        return res.status(404).send({ error: "Invalid password or email" });
     }
 });
 
