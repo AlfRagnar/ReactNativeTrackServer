10af09c37fb19d48524f44617fb9bd0bb45eb701
Alf-Ragnar Wirkola Masternes <alf-ragnar.wirkola@edunord.se>
2021-01-12 11:40:32 +0100

    Finished Chapter 14

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     const { authorization } = req.headers;
     // authorization === 'Bearer asdasdasjldjasdljasdjaskldj'
     if (!authorization || !authorization.includes("Bearer")) {
-        return res.status(401).send({ error: "Invalid authorization" });
+        return res.status(401).send({ error: "You must be logged in" });
     }
     const token = authorization.replace("Bearer ", "");
     jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
